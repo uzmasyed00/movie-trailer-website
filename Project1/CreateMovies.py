63f426800c2febed4d1cad9ae8692038943cc27c
uzmasyed00 <uzmasyed00@gmail.com>
2015-05-01 21:19:35 +1000

    resubmitting project 1 after changing code quality

--- Project1/CreateMovies.py
+++ Project1/CreateMovies.py
@@ -3,13 +3,31 @@ import fresh_tomatoes
 
 class Main():
     # Create instances of movie object in Movie class
-    DilWaleDulhaniyaLejayeinge = movie.Movie("Dil Wale Dulhaniya Lejayeinge","https://image.tmdb.org/t/p/original/pVGzV02qmHVoKx9ehBNy7m2u5fs.jpg","http://www.youtube.com/watch?v=1U9SpwJ9TCs", "A block buster in 1990's");
-    Lagaan = movie.Movie("Lagaan","http://media.movietalkies.com/posters/bollywood/movies/2001/lagaan/lagaan-2001-4b.jpg","http://www.youtube.com/watch?v=FNX1beRgwJ8","A must-watch for Amir Khan fans");
-    TwoStates = movie.Movie("Two States","https://pbs.twimg.com/profile_images/438944211245617152/cDjvFT3A.jpeg","http://www.youtube.com/watch?v=CGyAaR2aWcA","The best depiction between North Indian culture and South Indian culture");
-    ThreeIdiots = movie.Movie("Three Idiots","http://media1.santabanta.com/full1/Bollywood%20Movies/3%20Idiots/3-idiots-20h.jpg","http://www.youtube.com/watch?v=CGyAaR2aWcA","A must watch for Amir Khan fans");
+    DilWaleDulhaniyaLejayeinge = movie.Movie("Dil Wale Dulhaniya Lejayeinge",
+                                             "https://image.tmdb.org/t/p/original/pVGzV02qmHVoKx9ehBNy7m2u5fs.jpg",
+                                             "http://www.youtube.com/watch?v=1U9SpwJ9TCs",
+                                             "A block buster in 1990's")
+    
+    Lagaan = movie.Movie("Lagaan",
+                         "http://media.movietalkies.com/posters/bollywood/movies/2001/lagaan/lagaan-2001-4b.jpg",
+                         "http://www.youtube.com/watch?v=FNX1beRgwJ8",
+                         "A must-watch for Amir Khan fans")
+    
+    TwoStates = movie.Movie("Two States",
+                            "https://pbs.twimg.com/profile_images/438944211245617152/cDjvFT3A.jpeg",
+                            "http://www.youtube.com/watch?v=CGyAaR2aWcA",
+                            "The best depiction between North Indian culture and South Indian culture")
+    
+    ThreeIdiots = movie.Movie("Three Idiots",
+                              "http://media1.santabanta.com/full1/Bollywood%20Movies/3%20Idiots/3-idiots-20h.jpg",
+                              "https://www.youtube.com/watch?v=xvszmNXdM4w",
+                              "A must watch for Amir Khan fans")
 
     # Create a list of movies instantiated above
-    movies_list = [DilWaleDulhaniyaLejayeinge,Lagaan,TwoStates,ThreeIdiots];
+    movies_list = [DilWaleDulhaniyaLejayeinge,
+                   Lagaan,
+                   TwoStates,
+                   ThreeIdiots]
 
-    fresh_tomatoes.open_movies_page(movies_list);
+    fresh_tomatoes.open_movies_page(movies_list)
     
--- Project1/movie.py
+++ Project1/movie.py
@@ -1,8 +1,8 @@
 class Movie():
     def __init__(self, title, imageLink, trailerLink, description):
-        self.title = title;
-        self.poster_image_url = imageLink;
-        self.trailer_youtube_url = trailerLink;
-        self.description  = description;
+        self.title = title
+        self.poster_image_url = imageLink
+        self.trailer_youtube_url = trailerLink
+        self.description  = description
 
              
